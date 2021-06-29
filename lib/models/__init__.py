e435248559b685b161d16168764fad6e2c892063
swoook <kimseongwoo21@gmail.com>
2021-06-30 08:09:39 +0900

    Implementing a feature for training DDRNet23-slim on SOD datasets
    * Add a .yaml file for SOD model configuration
    * Fix importing style for compatibility

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -8,8 +8,14 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import models.seg_hrnet
-import models.seg_hrnet_ocr
-import models.ddrnet_23_slim
-import models.ddrnet_23
-import models.ddrnet_39
+from . import seg_hrnet
+from . import seg_hrnet_ocr
+from . import ddrnet_23_slim
+from . import ddrnet_23
+from . import ddrnet_39
+
+# import models.seg_hrnet
+# import models.seg_hrnet_ocr
+# import models.ddrnet_23_slim
+# import models.ddrnet_23
+# import models.ddrnet_39
