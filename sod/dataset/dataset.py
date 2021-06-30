cb5bc7b84f8ba2cbe3830ab0d9e7fbb329512e23
swoook <kimseongwoo21@gmail.com>
2021-06-30 17:06:38 +0900

    Implementing a feature for training DDRNet23-slim on SOD datasets
    * checked it's valid until L115 in solver.py

--- sod/dataset/dataset.py
+++ sod/dataset/dataset.py
@@ -3,38 +3,38 @@ from PIL import Image
 import cv2
 import torch
 from torch.utils import data
-from torchvision import transforms
-from torchvision.transforms import functional as F
-import numbers
 import numpy as np
 import random
+from lib.utils.utils import letterbox_resize
 
-class ImageDataTrain(data.Dataset):
-    def __init__(self, data_root, data_list):
-        self.sal_root = data_root
-        self.sal_source = data_list
 
-        with open(self.sal_source, 'r') as f:
-            self.sal_list = [x.strip() for x in f.readlines()]
+class DutsTr(data.Dataset):
+    def __init__(self, root_dir, list_txt_path):
+        self.root_dir = root_dir
+        self.list_txt_path = list_txt_path
 
-        self.sal_num = len(self.sal_list)
+        with open(self.list_txt_path, 'r') as f:
+            self.data_list = [x.strip() for x in f.readlines()]
+
+        self.num_data = len(self.data_list)
 
 
     def __getitem__(self, item):
         # sal data loading
-        im_name = self.sal_list[item % self.sal_num].split()[0]
-        gt_name = self.sal_list[item % self.sal_num].split()[1]
-        sal_image = load_image(os.path.join(self.sal_root, im_name))
-        sal_label = load_sal_label(os.path.join(self.sal_root, gt_name))
-        sal_image, sal_label = cv_random_flip(sal_image, sal_label)
-        sal_image = torch.Tensor(sal_image)
-        sal_label = torch.Tensor(sal_label)
+        im_name = self.data_list[item % self.num_data].split()[0]
+        gt_name = self.data_list[item % self.num_data].split()[1]
+        img = load_image(os.path.join(self.root_dir, im_name))
+        label = load_sal_label(os.path.join(self.root_dir, gt_name))
 
-        sample = {'sal_image': sal_image, 'sal_label': sal_label}
+        img, label = cv_random_flip(img, label)
+        img = torch.Tensor(img)
+        label = torch.Tensor(label)
+
+        sample = {'img': img, 'label': label}
         return sample
 
     def __len__(self):
-        return self.sal_num
+        return self.num_data
 
 class ImageDataTest(data.Dataset):
     def __init__(self, data_root, data_list):
@@ -55,16 +55,19 @@ class ImageDataTest(data.Dataset):
         return self.image_num
 
 
-def get_loader(args, mode='train', pin=False):
+def get_loader(config, mode='train', pin=False):
     shuffle = False
     if mode == 'train':
         shuffle = True
-        dataset = ImageDataTrain(args.train_root, args.train_list)
-        data_loader = data.DataLoader(dataset=dataset, batch_size=args.batch_size, shuffle=shuffle, num_workers=args.num_thread, pin_memory=pin)
+        dataset = DutsTr(config.DATASET.TRAIN_ROOT, config.DATASET.TRAIN_LIST)
+        data_loader = data.DataLoader(dataset=dataset, batch_size=config.TRAIN.BATCH_SIZE_PER_GPU, 
+        shuffle=shuffle, num_workers=config.WORKERS, pin_memory=pin)
     else:
-        dataset = ImageDataTest(args.test_root, args.test_list)
-        data_loader = data.DataLoader(dataset=dataset, batch_size=args.batch_size, shuffle=shuffle, num_workers=args.num_thread, pin_memory=pin)
+        dataset = ImageDataTest(config.DATASET.TEST_ROOT, config.DATASET.TEST_LIST)
+        data_loader = data.DataLoader(dataset=dataset, batch_size=config.TEST.BATCH_SIZE_PER_GPU, 
+        shuffle=shuffle, num_workers=config.WORKERS, pin_memory=pin)
     return data_loader
+
 
 def load_image(path):
     if not os.path.exists(path):
@@ -74,6 +77,7 @@ def load_image(path):
     in_ -= np.array((104.00699, 116.66877, 122.67892))
     in_ = in_.transpose((2,0,1))
     return in_
+
 
 def load_image_test(path):
     if not os.path.exists(path):
@@ -85,6 +89,7 @@ def load_image_test(path):
     in_ = in_.transpose((2,0,1))
     return in_, im_size
 
+
 def load_sal_label(path):
     if not os.path.exists(path):
         print('File {} not exists'.format(path))
@@ -95,6 +100,7 @@ def load_sal_label(path):
     label = label / 255.
     label = label[np.newaxis, ...]
     return label
+
 
 def cv_random_flip(img, label):
     flip_flag = random.randint(0, 1)
--- sod/solver.py
+++ sod/solver.py
@@ -1,35 +1,30 @@
 import torch
-from collections import OrderedDict
-from torch.nn import utils, functional as F
+from torch.nn import functional as F
 from torch.optim import Adam
 from torch.autograd import Variable
 from torch.backends import cudnn
-from sod.networks.poolnet import build_model, weights_init
 from sod.dataset.dataset import load_image_test
-import scipy.misc as sm
 import numpy as np
 import os
-import torchvision.utils as vutils
 import cv2
-import math
 import time
-
 import lib.models
-from lib.config import config_sod_ddrnet_23_slim
 
 
 class Solver(object):
-    def __init__(self, train_loader, test_loader, args):
+    def __init__(self, train_loader, test_loader, config, args):
         self.train_loader = train_loader
         self.test_loader = test_loader
-        self.config = config_sod_ddrnet_23_slim()
-        self.config.merge_from_file(args.cfg_path)
-        self.device = torch.device('cuda' if args.cuda else 'cpu')
-        self.acc_step_size = args.acc_step_size
-        self.show_every = args.show_every
+        self.args = args
+        self.config = config
+        self.device = torch.device(
+            'cuda:{}'.format(self.args.local_rank) if ((len(self.config.GPUS) > 0) & (torch.cuda.is_available())) 
+            else 'cpu')
+        self.grad_accumulation_step_size = self.config.TRAIN.GRAD_ACCUMULATION_STEP_SIZE
+        self.print_freq = self.config.TRAIN.PRINT_FREQ
         self.lr_decay_epoch = [15,]
         self.build_model()
-        self.print_network(self, self.net, 'DDRNet')
+        self.print_network(self.net, 'DDRNet')
 
     # print the network information and parameter numbers
     def print_network(self, model, name):
@@ -42,19 +37,25 @@ class Solver(object):
 
     # build the network
     def build_model(self):
-        if self.args.cuda:
+        if self.device.type == 'cuda':
             cudnn.benchmark = self.config.CUDNN.BENCHMARK
             cudnn.deterministic = self.config.CUDNN.DETERMINISTIC
             cudnn.enabled = self.config.CUDNN.ENABLED
+            torch.cuda.set_device(self.device)
+            torch.distributed.init_process_group(
+                backend="nccl", init_method="env://",)  
 
         # build model
         if torch.__version__.startswith('1'):
             module = lib.models.__dict__[self.config.MODEL.NAME]
+            ''' TODO
+            torch.nn.SyncBatchNorm requires calling `torch.distributed.init_process_group` while training
+            '''
             module.BatchNorm2d_class = module.BatchNorm2d = torch.nn.SyncBatchNorm    
-        self.net = lib.models.__dict__[self.config.MODEL.NAME].__dict__['get_seg_model'](self.config).to(self.device)
+        self.net = module.__dict__['get_seg_model'](self.config, pretrained=False).to(self.device)
 
         # load pre-trained weights
-        pretrained_dict = torch.load(self.args.TEST.MODEL_FILE, map_location=self.device)
+        pretrained_dict = torch.load(self.config.MODEL.PRETRAINED, map_location=self.device)
         if 'state_dict' in pretrained_dict:
             pretrained_dict = pretrained_dict['state_dict']
         model_dict = self.net.state_dict()
@@ -87,9 +88,7 @@ class Solver(object):
         for i, data_batch in enumerate(self.test_loader):
             images, name, im_size = data_batch['image'], data_batch['name'][0], np.asarray(data_batch['size'])
             with torch.no_grad():
-                images = Variable(images)
-                if self.args.cuda:
-                    images = images.cuda()
+                images = Variable(images).to(self.device)
                 preds = self.net(images)
                 pred = np.squeeze(torch.sigmoid(preds).cpu().data.numpy())
                 multi_fuse = 255 * pred
@@ -100,20 +99,18 @@ class Solver(object):
 
 
     def train(self):
-        iter_num = len(self.train_loader.dataset) // self.args.batch_size
+        iter_num = len(self.train_loader.dataset) // self.config.TRAIN.BATCH_SIZE_PER_GPU
         aveGrad = 0
-        for epoch in range(self.args.epoch):
+        for epoch in range(self.config.TRAIN.BEGIN_EPOCH, self.config.TRAIN.END_EPOCH):
             r_sal_loss= 0
             self.net.zero_grad()
             for i, data_batch in enumerate(self.train_loader):
-                sal_image, sal_label = data_batch['sal_image'], data_batch['sal_label']
+                sal_image, sal_label = data_batch['img'], data_batch['label']
                 if (sal_image.size(2) != sal_label.size(2)) or (sal_image.size(3) != sal_label.size(3)):
                     print('IMAGE ERROR, PASSING```')
                     continue
                 sal_image, sal_label= Variable(sal_image), Variable(sal_label)
-                if self.args.cuda:
-                    # cudnn.benchmark = True
-                    sal_image, sal_label = sal_image.cuda(), sal_label.cuda()
+                sal_image, sal_label = sal_image.to(self.device), sal_label.to(self.device)
 
                 sal_pred = self.net(sal_image)
                 sal_loss_fuse = F.binary_cross_entropy_with_logits(sal_pred, sal_label, reduction='sum')
@@ -125,7 +122,7 @@ class Solver(object):
                 So, it divides the losses by not only batch size but also $self.iter_size
                 However, it seems the losses are averaged across observations for each minibatch if `reduction='mean'`
                 '''
-                sal_loss = sal_loss_fuse / (self.acc_step_size * self.args.batch_size)
+                sal_loss = sal_loss_fuse / (self.grad_accumulation_step_size * self.args.batch_size)
                 r_sal_loss += sal_loss.data
 
                 sal_loss.backward()
@@ -139,12 +136,12 @@ class Solver(object):
                 Find gradient accumulation for more details
                 https://makinarocks.github.io/Gradient-Accumulation/
                 '''
-                if aveGrad % self.acc_step_size == 0:
+                if aveGrad % self.grad_accumulation_step_size == 0:
                     self.optimizer.step()
                     self.optimizer.zero_grad()
                     aveGrad = 0
 
-                if i % (self.show_every // self.args.batch_size) == 0:
+                if i % (self.print_freq // self.args.batch_size) == 0:
                     if i == 0:
                         x_showEvery = 1
                     print('epoch: [%2d/%2d], iter: [%5d/%5d]  ||  Sal : %10.4f' % (
